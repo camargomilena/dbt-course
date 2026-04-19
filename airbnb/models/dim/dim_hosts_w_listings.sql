aa4a88f3d4a2765d05baaba08f0cac1b9389702e
camargomilena <milenacamarggo@gmail.com>
2026-04-19 13:05:58 -0300

    init: configure dbt models and materialization strategy

---
+++ airbnb/models/dim/dim_hosts_w_listings.sql
@@ -0,0 +1,20 @@
+WITH h AS (
+    SELECT * FROM {{ref('dim_hosts_cleansed')}}
+),
+l AS (
+    SELECT * FROM {{ref('dim_listings_cleansed')}}
+)
+SELECT 
+    l.LISTING_ID,
+    l.LISTING_URL, 
+    l.LISTING_NAME,
+    l.ROOM_TYPE,
+    l.MINIMUM_NIGHTS,
+    l.HOST_ID,
+    l.PRICE,
+    l.CREATED_AT,
+    h.HOST_NAME,
+    h.IS_SUPERHOST as HOST_IS_SUPERHOST,
+    GREATEST(l.UPDATED_AT, h.UPDATED_AT) as UPDATED_AT
+FROM l
+LEFT JOIN h on (h.HOST_ID = l.HOST_ID)  
